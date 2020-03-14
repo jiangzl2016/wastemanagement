3c7522242f5e9d42a1b37ae8c21e80d06f91c214
Zhongling Jiang <zj2249@columbia.edu>
2020-03-14 17:53:25 -0500

    install postgres; migrate to postgres

--- wastemanagement/wastemanagement/settings.py
+++ wastemanagement/wastemanagement/settings.py
@@ -84,9 +84,17 @@ WSGI_APPLICATION = 'wastemanagement.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',  ## TODO: Use PostgresSQL instead
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  ## TODO: Use PostgresSQL instead
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wastetracking',
+        'USER': 'postgres',
+        'PASSWORD': 'wastemanagementpw',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
